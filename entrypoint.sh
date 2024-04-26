44bf98312ac850354d6f12e94a2ee41978c0aec5
FoxxMD <FoxxMD@users.noreply.github.com>
2024-04-26 15:34:37 -0400

    second pass while checking if driver is installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,35 +41,53 @@ fi
 unset IFS
 set +f
 
-# assume user does not want to install plugin
-PLUGIN="${PLUGIN_VERSION:=false}"
-if [ "$PLUGIN" != "false" ]; then
+if [ $(dpkg-query -W -f='${Status}' libsane-hpaio 2>/dev/null | grep -c "ok installed") -eq 1 ];
+then
+  # assume user does not want to install plugin
+  HP_PLUGIN="${HP_PLUGIN:=false}"
+  if [ "$PLUGIN" != "false" ]; then
+    echo "Checking HP_PLUGIN status..."
+    # determine installed HPLIP version
+    RE="HP Linux Imaging and Printing System \(ver\. (.+?)\)"
+    cmd_output=$(hp-plugin --help 2>&1)
+    #https://stackoverflow.com/a/2778096
+    RAW_VERSION="$(echo "$cmd_output" | sed -rn "s/$RE/\1/p")"
+    # Remove ansi coloring so its just a raw string
+    #https://stackoverflow.com/a/51141872
+    HPLIP_VERSION=$(echo "$RAW_VERSION" | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
+    printf 'HPLIP Version: %s\n' "$HPLIP_VERSION"
 
-  # determine installed HPLIP version
-  RE="HP Linux Imaging and Printing System \(ver\. (.+?)\)"
-  cmd_output=$(hp-plugin --help 2>&1)
-  #https://stackoverflow.com/a/2778096
-  HPLIP_VERSION="$(echo "$cmd_output" | sed -rn "s/$RE/\1/p")"
-  printf 'HPLIP Version: %s\n' "HPLIP_VERSION"
+    # if variable is true then try to determine plugin version automatically
+    if [ "$HP_PLUGIN" = "true" ]; then
 
-  # if variable is true then try to determine plugin version automatically
-  if [ "$PLUGIN" = "true" ]; then
-
-    # check if plugin is already installed
-    # files installed to these locations https://wiki.gentoo.org/wiki/HPLIP#Binary_plugins
-    if [ -d /usr/share/hplip/data/firmware ]; then
-      printf 'A plugin is already installed. To force (re)install specify version %s\n' "HPLIP_VERSION"
+      # check if plugin is already installed
+      # files installed to these locations https://wiki.gentoo.org/wiki/HPLIP#Binary_plugins
+      if [ -d /usr/share/hplip/data/firmware ]; then
+        printf 'A plugin is already installed. To force (re)install specify version in ENV like HP_PLUGIN=%s\n' "$HPLIP_VERSION"
+      else
+        INSTALL_PLUGIN_VERSION=$HPLIP_VERSION
+      fi
     else
-      INSTALL_PLUGIN_VERSION=HPLIP_VERSION
+      INSTALL_PLUGIN_VERSION=$HPLIP_VERSION
     fi
   else
-    INSTALL_PLUGIN_VERSION=HPLIP_VERSION
+    echo 'HP Driver installed but HP_PLUGIN ENV not invoked'
   fi
-fi
 
-if [ ! -z "$INSTALL_PLUGIN_VERSION" ]; then
-  print 'Attempting to install plugin version %s\n' "INSTALL_PLUGIN_VERSION"
-  wget -P /tmp
+  if [ ! -z "$INSTALL_PLUGIN_VERSION" ]; then
+    printf 'Attempting to install plugin version %s\n' "$INSTALL_PLUGIN_VERSION"
+    PLUGIN_FILE="/tmp/hplip-$INSTALL_PLUGIN_VERSION-plugin.run"
+
+    if [ ! -f "$PLUGIN_FILE" ]; then
+        echo 'Plugin does not already existing, downloading...'
+        wget --backups 0 -P /tmp "https://developers.hp.com/sites/default/files/hplip-$INSTALL_PLUGIN_VERSION-plugin.run"
+    fi
+    chmod +x "$PLUGIN_FILE"
+    yes | "$PLUGIN_FILE" --noprogress --accept --nox11 -- -i
+    echo "Plugin installed!"
+  fi
+else
+  echo 'HP driver not installed, no plugin check required.'
 fi
 
 
