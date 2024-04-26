937903fcde18c48fc3b926830caea728402255c7
FoxxMD <FoxxMD@users.noreply.github.com>
2024-04-26 14:44:59 -0400

    first pass at identifying plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,4 +41,36 @@ fi
 unset IFS
 set +f
 
+# assume user does not want to install plugin
+PLUGIN="${PLUGIN_VERSION:=false}"
+if [ "$PLUGIN" != "false" ]; then
+
+  # determine installed HPLIP version
+  RE="HP Linux Imaging and Printing System \(ver\. (.+?)\)"
+  cmd_output=$(hp-plugin --help 2>&1)
+  #https://stackoverflow.com/a/2778096
+  HPLIP_VERSION="$(echo "$cmd_output" | sed -rn "s/$RE/\1/p")"
+  printf 'HPLIP Version: %s\n' "HPLIP_VERSION"
+
+  # if variable is true then try to determine plugin version automatically
+  if [ "$PLUGIN" = "true" ]; then
+
+    # check if plugin is already installed
+    # files installed to these locations https://wiki.gentoo.org/wiki/HPLIP#Binary_plugins
+    if [ -d /usr/share/hplip/data/firmware ]; then
+      printf 'A plugin is already installed. To force (re)install specify version %s\n' "HPLIP_VERSION"
+    else
+      INSTALL_PLUGIN_VERSION=HPLIP_VERSION
+    fi
+  else
+    INSTALL_PLUGIN_VERSION=HPLIP_VERSION
+  fi
+fi
+
+if [ ! -z "$INSTALL_PLUGIN_VERSION" ]; then
+  print 'Attempting to install plugin version %s\n' "INSTALL_PLUGIN_VERSION"
+  wget -P /tmp
+fi
+
+
 node ./server/server.js
